6f3863e4e81b123b75f67da692eb916a506ad2b2
hanumangayalkoti <hanumangayalkoti@gmail.com>
2026-05-17 08:35:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+API_KEY = os.getenv("API_KEY", "")
+API_SECRET = os.getenv("API_SECRET", "")
+API_PARTNER_TAG = os.getenv("API_PARTNER_TAG", "")
